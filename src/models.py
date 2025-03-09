0803de73bcd81851d18fe258d40540e0b6383770
AleKolar <157892507+AleKolar@users.noreply.github.com>
2025-03-09 17:24:25 +0300

    models_&_validators_fixes-&_manual_testing_1

--- src/models.py
+++ src/models.py
@@ -40,7 +40,8 @@ class User(BaseModel):
 
             # Генерируем расписание
             for day in range(drug.duration_days):
-                current_date = first_time_rounded.date() + timedelta(days=day)
+                current_date = first_time_rounded.date() + timedelta(days=day) # Тут я запутался и прибегнул
+                # к помощи "индийского программиста", но вся логика моя от и до(может и не совсем верная)
 
                 # Устанавливаем начальное время для текущего дня
                 if day == 0:
